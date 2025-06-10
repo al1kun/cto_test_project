defbaf85e8d00050dc6b102b4097cd6f605ba35e
Alikhan <a.alikhan2002@gmail.com>
2025-06-11 02:59:53 +0500

    fix(core): delete scanBasePackage - "common"

--- core/src/main/java/com/example/sto/core/StoApplication.java
+++ core/src/main/java/com/example/sto/core/StoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
-        "com.example.sto.common",
         "com.example.sto.core",
         "com.example.sto.notification"
 })
